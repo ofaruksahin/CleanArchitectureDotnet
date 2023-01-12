be1e123a2a3ee9963128fe2c3d51bf5fb7d87651
ofaruksahin@outlook.com.tr <ofaruksahin@outlook.com.tr>
2023-01-12 23:50:20 +0300

    Repositorylerin implement edilmesi

---
+++ CleanArchitectureDotnet.Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitectureDotnet.Application.Contracts.BaseConstracts;
+
+namespace CleanArchitectureDotnet.Persistence.Repositories
+{
+    public class BaseRepository : IBaseRepository
+    {
+        public BaseDbContext _dbContext;
+
+        public BaseRepository(BaseDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
---
+++ CleanArchitectureDotnet.Presentation/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CleanArchitectureDotnet.Application.Contracts.BaseConstracts;
+using CleanArchitectureDotnet.Persistence;
+using CleanArchitectureDotnet.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitectureDotnet.Presentation.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddDbContext<BaseDbContext>(options =>
+            {
+
+            });
+
+            services.AddScoped<IBaseRepository, BaseRepository>();
+            return services;
+        }
+    }
+}
